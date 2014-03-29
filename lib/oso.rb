37922b172b7438e5e64ade160cf6cf928450ad2c
Ken-Lauren Daganio <akosiken@gmail.com>
2014-03-29 15:48:06 +0800

    Adds oso code

--- lib/oso.rb
+++ lib/oso.rb
@@ -1,5 +1,43 @@
 require "oso/version"
+require 'net/http'
+require 'uri'
 
 module Oso
-  # Your code goes here...
+
+  oso = <<STRING
+                  
+        ("-^-/")  
+        `o__o' ]  
+        (_Y_) _/  
+      _..`--'-.`, 
+     (__)_,--(__) 
+         7:   ; 1 
+       _/,`-.-' : 
+      (_,)-~~(_,) 
+STRING
+
+  uri = URI.parse('http://iheartquotes.com/api/v1/random')
+  response = Net::HTTP.get_response(uri)
+  quote = response.body
+
+  lines = quote.split(/[\r\n]+/)
+  lines = lines[0...-1]
+  longest = lines.map(&:length).max
+  oso_in_lines = oso.split(/[\r\n]+/)
+
+  last_index = 0
+  oso_in_lines[0] <<  "  " + "_" * (longest + 4)
+
+  lines.each_with_index do |line, i|
+    pad = ((longest - line.strip.length) / 2).to_i
+    oso_in_lines[i+1] << "  |"
+    oso_in_lines[i+1] <<  " " * (pad + 1)
+    oso_in_lines[i+1] <<  line.strip
+    oso_in_lines[i+1] <<  " " * (pad + 1)
+    oso_in_lines[i+1] << "|"
+    last_index = i
+  end
+  oso_in_lines[last_index + 2] <<  "  " + "_" * (longest + 4)
+
+  puts oso_in_lines
 end
--- lib/oso/version.rb
+++ lib/oso/version.rb
@@ -1,3 +1,3 @@
 module Oso
-  VERSION = "0.0.1"
+  VERSION = "0.0.9001"
 end
